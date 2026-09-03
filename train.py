fa2c6183c0430b2652c9f2c8024ff2b9deb7132f
Артемий Сунгуров <raknagibator3@gamil.com>
2026-09-03 23:14:15 +0300

    change params

--- train.py
+++ train.py
@@ -12,10 +12,10 @@ from sklearn.tree import DecisionTreeClassifier
 def main():
     X, y = load_iris(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42
+        X, y, test_size=0.1, random_state=42
     )
 
-    model = DecisionTreeClassifier(max_depth=8, random_state=42)
+    model = DecisionTreeClassifier(max_depth=2, random_state=42)
     model.fit(X_train, y_train)
 
     score = model.score(X_test, y_test)
